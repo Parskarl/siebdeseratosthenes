ae30f6ecea0fee1eca3a70e775f1a49db830332b
Parskarl <pasci17.3.2006@gmail.com>
2022-09-17 10:58:09 +0200

    3. Commit - print Primes

--- src/net/htlgkr/SaurerP190201/siebdeseratosthenes/EratosthenesPrimeSieve.java
+++ src/net/htlgkr/SaurerP190201/siebdeseratosthenes/EratosthenesPrimeSieve.java
@@ -1,21 +1,28 @@
 package net.htlgkr.SaurerP190201.siebdeseratosthenes;
 
+import java.util.ArrayList;
+
 public class EratosthenesPrimeSieve implements PrimeSieve{
 
     int obergrenze;
+    ArrayList<Integer> primes = new ArrayList<>();
     public EratosthenesPrimeSieve(int o)
     {
         this.obergrenze=o;
     }
 
     @Override
-    public boolean isPrime(int p) {
+    public boolean isPrime(int p)
+    {
         return false;
     }
 
     @Override
     public void printPrimes()
     {
-
+        for(int p:primes)
+        {
+            System.out.println(p);
+        }
     }
 }
